dcc8d5773b6abb74535c1efc3dbd98c4a757bbbc
alexei-kouprianov <alexei.kouprianov@gmail.com>
2020-03-29 05:37:32 +0300

    Rospotrebnadzor reports reviewed; Healed data added; Plots redrawn;

--- scripts/covid.2019.ru.main.r
+++ scripts/covid.2019.ru.main.r
@@ -2,6 +2,7 @@
 # Loading libraries, setting locale for time
 
 library(maps)
+library(png)
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
 
 ################################################################
@@ -10,6 +11,8 @@ Sys.setlocale("LC_TIME", "en_US.UTF-8")
 covid.2019.ru <- read.table("../data/momentary.txt", h=TRUE, sep="\t")
 covid.2019.breaks <- read.table("../misc/breaks.txt", h=TRUE, sep="\t")
 covid.2019.coord <- read.table("../misc/coord.txt", h=TRUE, sep="\t")
+# cc.logo <- readPNG("../misc/240px-Cc.logo.circle.svg.png")
+# cc_by.logo <- readPNG("../misc/Cc-by_new.svg.png")
 
 ################################################################
 # Disaggregating
@@ -42,14 +45,14 @@ covid.2019.ru.da.h <- subset(covid.2019.ru.da, covid.2019.ru.da$EVENT == "healed
 ################################################################
 # Data transformations
 
-# Timeseries list
+# Timeseries list;
 covid.2019.ru.i.ts <- NULL
 
 for(i in 1:length(levels(covid.2019.ru.i$TIMESTAMP))){
 covid.2019.ru.i.ts[[i]] <- subset(covid.2019.ru.i, covid.2019.ru.i$TIMESTAMP == levels(covid.2019.ru.i$TIMESTAMP)[i])
 }
 
-# Noncumulated true time timeseries list
+# Noncumulated true time timeseries list;
 covid.2019.ru.i.moment_ts <- NULL
 
 for(i in 1:length(covid.2019.breaks$TIME)){
@@ -93,7 +96,7 @@ sum(covid.2019.ru.i.ts[[i]]$NUMBER))
 colnames(covid.2019.ru.i.dyn) <- c("TIME","NUMBER")
 covid.2019.ru.i.dyn$CUMSUM <- cumsum(covid.2019.ru.i.dyn$NUMBER)
 
-# Barplot regions data frame
+# Barplot regions data frame;
 covid.2019.ru.i.reg.df <- NULL
 
 for(i in 1:length(levels(covid.2019.ru.i$LOCUS))){
@@ -106,7 +109,7 @@ sum(covid.2019.ru.i.reg[[i]]$NUMBER))
 colnames(covid.2019.ru.i.reg.df) <- c("LOCUS","NUMBER")
 covid.2019.ru.i.reg.ordered.df <- covid.2019.ru.i.reg.df[order(-covid.2019.ru.i.reg.df$NUMBER),]
 
-# Mapping regions data frame
+# Mapping regions data frame;
 covid.2019.ru.i.reg.0.df <- NULL
 
 for(i in 1:length(levels(covid.2019.ru.i$LOCUS.0))){
@@ -218,8 +221,8 @@ axis.POSIXct(1,
 at=seq(min(covid.2019.breaks$TIME), max(covid.2019.breaks$TIME), by="week"), 
 format = "%Y-%m-%d", 
 las=2)
-axis(2, at=log10(c(1,10,100)), labels=c(1,10,100))
-axis(2, at=log10(c(1:10, seq(10,100,10), seq(100,1000,100))), labels=FALSE)
+axis(2, at=log10(c(1,10,100,1000)), labels=c(1,10,100,1000))
+axis(2, at=log10(c(1:9, seq(10,100,10), seq(200,1000,100), seq(2000,10000,1000))), labels=FALSE)
 
 dev.off()
 
@@ -237,12 +240,18 @@ axes=FALSE)
 points(covid.2019.ru.i.dyn.tt$TIME, log10(covid.2019.ru.i.dyn.tt$SPb.CS), type="l",col=4)
 points(covid.2019.ru.i.dyn.tt$TIME, log10(covid.2019.ru.i.dyn.tt$RUS.CS - (covid.2019.ru.i.dyn.tt$Mos.CS + covid.2019.ru.i.dyn.tt$SPb.CS)), type="l",col=3)
 
+rasterImage(cc.logo, 
+xleft = covid.2019.ru.i.dyn.tt$TIME[1], 
+xright=covid.2019.ru.i.dyn.tt$TIME[1]+dim(cc.logo)[2]*1200, 
+ytop = log10(100), 
+ybottom=log10(100)-dim(cc.logo)[1]/1600)
+
 axis.POSIXct(1, 
 at=seq(min(covid.2019.breaks$TIME), max(covid.2019.breaks$TIME), by="week"), 
 format = "%Y-%m-%d", 
 las=2)
-axis(2, at=log10(c(1,10,100)), labels=c(1,10,100))
-axis(2, at=log10(c(1:10, seq(10,100,10), seq(100,1000,100))), labels=FALSE)
+axis(2, at=log10(c(1,10,100,1000)), labels=c(1,10,100,1000))
+axis(2, at=log10(c(1:9, seq(10,100,10), seq(200,1000,100), seq(2000,10000,1000))), labels=FALSE)
 
 legend("topleft", lt=1, col=c(2,4,3), legend=c("Moscow","St. Petersburg","The rest of Russia"), bty="n")
 
@@ -269,7 +278,7 @@ mtext(paste("Total COVID-2019 cases, as of",covid.2019.ru.i$TIMESTAMP[length(cov
 side=1, line=2) 
 mtext("Russian Federation", font=2, cex=1.2, side=3, line=3)
 
-points(covid.2019.coord$LON, covid.2019.coord$LAT, cex=sqrt(covid.2019.ru.i.reg.0.df$NUMBER/2), pch=21, bg=2)
+points(covid.2019.coord$LON, covid.2019.coord$LAT, cex=sqrt(covid.2019.ru.i.reg.0.df$NUMBER)/2, pch=21, bg=2)
 
 dev.off()
 
@@ -292,7 +301,7 @@ dev.off()
 # side=1, line=2, cex=2) 
 # mtext("Russian Federation", font=2, cex=2, side=3, line=3)
 #
-# points(covid.2019.coord$LON, covid.2019.coord$LAT, cex=sqrt(as.data.frame(table(covid.2019.ru.i.cumul_ts[[i]]$LOCUS.0))$Freq/2), pch=21, bg=2)
+# points(covid.2019.coord$LON, covid.2019.coord$LAT, cex=sqrt(as.data.frame(table(covid.2019.ru.i.cumul_ts[[i]]$LOCUS.0))$Freq)/2, pch=21, bg=2)
 #
 # dev.off()
 # }
