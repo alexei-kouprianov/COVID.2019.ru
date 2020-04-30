1203cd92d493ccba2d67c724cef5bc2f689edd89
alexei-kouprianov <alexei.kouprianov@gmail.com>
2020-04-30 13:00:53 +0300

    Updated to 2020-04-30;

--- scripts/covid.2019.ru.main.r
+++ scripts/covid.2019.ru.main.r
@@ -7,6 +7,7 @@ source("covid.2019.ru.data_loader.r")
 # Running analytic and plotting scripts
 
 source("covid.2019.ru.data_transformations.r")
+render("../Rmd/daily.report.Rmd")
 source("covid.2019.ru.plots.r")
 # source("covid.2019.ru.growth.models.r")
 
